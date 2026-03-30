8c6d2596ace77a7509d5a26aac2d5d0f06d8d466
Marcotian0211 <350327743@tdsb.ca>
2026-03-30 02:04:51 +0000

    update practice problem3-4

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,51 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int min(int num,int num1,int num2){
+		if (num>num1&& num1>num2){
+			return num2;
+		}
+		else if(num>num1&& num1 <num2){
+         return num1;
+		}
+		else if(num<num1&&num1<num2){
+			return num;
+		}
+		else if(num<num1&&num1>num2&&num<num2){
+			return num;
+		}
+		else{
+			return num;
 
+		}
+			
+		
+	}
+	public static Boolean isLeapYear(int num){
+{
+    if(num % 4 == 0){            
+    if(num % 100 == 0){        
+    if(num % 400 == 0){   
+        return true; }       
+     else {
+                return false;       
+            }}
+        else {
+            return true;           
+        }}
+     else {
+        return false;               
+    }
 }
+	}
+}
+           
+	         
+	
+			
+
+	
+
+	
+
+
